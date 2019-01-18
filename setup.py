1cabfd9485404346a4df382dfb3f4cf468797379
renardeinside <polarpersonal@gmail.com>
2019-01-18 14:39:12 +0300

    Add new descriptions to setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,27 @@ URL = 'http://scikit-learn.org'
 DOWNLOAD_URL = 'https://pypi.org/project/lastochka/#files'
 LICENSE = 'MIT'
 
+
+CLASSIFIERS = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+]
+
 setup(
     name="lastochka",
     version=__version__,
     description="Lastochka - Weight Of Evidence Transformer",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/rst',
     author=MAINTAINER,
     url='https://github.com/sberbank-ai/lastochka',
     license=LICENSE,
